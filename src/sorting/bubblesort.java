b8a9984612f68f5483c317cfe0ce2044e6b1d357
kothuru venkata varshith kumar <kothuruvarshith@gmail.com>
2020-01-30 06:27:00 +0530

    merge and commit for sorting

--- src/sorting/bubblesort.java
+++ src/sorting/bubblesort.java
@@ -26,7 +26,7 @@ public class bubblesort {
 	                }  
 	                System.out.println();  
 	                  
-	                bubbleSort(arr);//sorting array elements using bubble sort  
+	                bubbleSort(arr); 
 	                 
 	                System.out.println("Array After Bubble Sort");  
 	                for(int i=0; i < arr.length; i++){  
